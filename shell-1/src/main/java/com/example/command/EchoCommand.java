077287475cc2302651dba7258f65b306ebc70629
ofer.karp <ofer.karp@gmail.com>
2020-01-05 18:01:30 +0200

    shell-1

---
+++ shell-1/src/main/java/com/example/command/EchoCommand.java
@@ -0,0 +1,26 @@
+package com.example.command;
+
+import com.example.shell.ShellHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+public class EchoCommand {
+
+    @Autowired
+    ShellHelper shellHelper;
+
+    @ShellMethod("Displays greeting message to the user whose name is supplied")
+    public String echo(@ShellOption({"-N", "--name"}) String name) {
+        String message = String.format("Hello %s!", name);
+        shellHelper.print(message.concat(" (Default style message)"));
+        shellHelper.printError(message.concat(" (Error style message)"));
+        shellHelper.printWarning(message.concat(" (Warning style message)"));
+        shellHelper.printInfo(message.concat(" (Info style message)"));
+        shellHelper.printSuccess(message.concat(" (Success style message)"));
+        String output = shellHelper.getSuccessMessage(message);
+        return output.concat(" You are running spring shell cli-demo.");
+    }
+}
