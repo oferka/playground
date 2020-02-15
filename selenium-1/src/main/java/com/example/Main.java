3ef46cd08d1e6d56d002366dba5f85cc9e033bd1
ofer.karp <ofer.karp@gmail.com>
2020-02-15 16:13:06 +0200

    selenium-1

---
+++ selenium-1/src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        driver.close();
+    }
+}
